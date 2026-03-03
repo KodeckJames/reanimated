fc736b4a5c4b99064bc473124b3bf96e760c2691
JJ <153640426+KodeckJames@users.noreply.github.com>
2026-03-03 19:13:32 +0300

    Feat: Configured first opacity transition animation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,100 +1,29 @@
-import { Image } from 'expo-image'
-import { Platform, StyleSheet } from 'react-native'
-
-import { HelloWave } from '@/components/hello-wave'
-import ParallaxScrollView from '@/components/parallax-scroll-view'
-import { ThemedText } from '@/components/themed-text'
-import { ThemedView } from '@/components/themed-view'
-import { Link } from 'expo-router'
+import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
 import { AnimatedThemeToggle } from '@/components/ThemeToggle'
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView className="flex-row items-center gap-2">
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-        <AnimatedThemeToggle />
-      </ThemedView>
-      <ThemedView className="gap-2 mb-2">
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{' '}
-          to see changes. Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView className="gap-2 mb-2">
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction
-              title="Action"
-              icon="cube"
-              onPress={() => alert('Action pressed')}
-            />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+import Animated, {useAnimatedStyle, useSharedValue, withTiming} from 'react-native-reanimated'
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView className="gap-2 mb-2">
-        <ThemedText type="subtitle">
-          Step 3: Build with Semantic Colors
-        </ThemedText>
-        <ThemedText>
-          {`Instead of hardcoding colors, use classes like `}
-          <ThemedText type="defaultSemiBold">bg-card</ThemedText> {'or '}
-          <ThemedText type="defaultSemiBold">text-primary.</ThemedText>{' '}
-          {`They automatically switch between light and dark modes based on your `}
-          <ThemedText type="defaultSemiBold">tailwind.config.js</ThemedText>{' '}
-          settings.{' '}
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+export default function Home() {
+
+  const opacity = useSharedValue(0);
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value
+    }
+  })
+  const fadeIn = () => {
+    opacity.value=withTiming(1, {duration: 2000})
+  }
+
+  useEffect(() => {
+    fadeIn()
+  },[])
+
+  return (
+    <View className=' min-h-screen flex justify-center items-center'>
+      <AnimatedThemeToggle className=' absolute left-4 top-4' />
+      <Animated.View style={animatedStyle} className=' bg-orange-500 w-24 h-24 rounded-full'/>
+    </View>
   )
 }
-
-const styles = StyleSheet.create({
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-})
