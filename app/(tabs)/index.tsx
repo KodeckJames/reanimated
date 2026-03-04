ceefbb7cffcb47e46dc85ad841fd6390591ce939
JJ <153640426+KodeckJames@users.noreply.github.com>
2026-03-04 07:19:35 +0300

    Feat: backgroundColor with interpolation between number and string shared values

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React, { useEffect } from 'react'
 import { AnimatedThemeToggle } from '@/components/ThemeToggle'
 
-import Animated, {useAnimatedStyle, useSharedValue, withTiming} from 'react-native-reanimated'
+import Animated, {useAnimatedStyle, useSharedValue, withTiming, interpolateColor} from 'react-native-reanimated'
 
 export default function Home() {
 
+  // Opacity Example
   const opacity = useSharedValue(0);
   const animatedStyle = useAnimatedStyle(() => {
     return {
@@ -13,17 +14,31 @@ export default function Home() {
     }
   })
   const fadeIn = () => {
-    opacity.value=withTiming(1, {duration: 2000})
+    opacity.value=withTiming(1, {duration: 5000})
   }
 
   useEffect(() => {
     fadeIn()
-  },[])
+  }, [])
+  
+  // Background color Example
+  const bgColor = useSharedValue(0);
+  const animatedBg = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(bgColor.value, [0, 1], ['red', 'purple']),
+    }
+  })
+  const handlePress = () => {
+    bgColor.value = withTiming(bgColor.value === 0 ? 1 : 0, { duration: 1000 });
+  }
 
   return (
     <View className=' min-h-screen flex justify-center items-center'>
       <AnimatedThemeToggle className=' absolute left-4 top-4' />
       <Animated.View style={animatedStyle} className=' bg-orange-500 w-24 h-24 rounded-full'/>
+      <Pressable onPress={()=>handlePress()}>
+        <Animated.View style={animatedBg} className=' w-24 h-24 rounded-full mt-4' />
+      </Pressable>
     </View>
   )
 }
