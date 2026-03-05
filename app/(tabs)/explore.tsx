2c0c33970a26af1a04befca011ed020c3bd08cf9
JJ <153640426+KodeckJames@users.noreply.github.com>
2026-03-05 16:59:59 +0300

    stopping, pausing & resuming animations

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,8 +1,14 @@
 import { useEffect } from 'react';
-import { View } from 'react-native';
-import Animated, { useAnimatedStyle, useSharedValue, withDelay, withRepeat, withSequence, withTiming } from 'react-native-reanimated';
+import { View, Pressable, Text } from 'react-native';
+import Animated, { cancelAnimation, useAnimatedStyle, useSharedValue, withDelay, withRepeat, withSequence, withTiming } from 'react-native-reanimated';
+import { withPause } from 'react-native-redash';
 
 export default function ExplorePage() {
+   // pause animation
+  const paused = useSharedValue(false);
+  const toggle = () => {
+    paused.value = !paused.value;
+  }
   // withSequence API with TranslateY
   const TranslateY = useSharedValue(0);
   const animTranslate = useAnimatedStyle(() => {
@@ -11,18 +17,32 @@ export default function ExplorePage() {
     }
   })
   const TranslateFunc = () => {
-    TranslateY.value = withDelay(3000,withRepeat(withSequence(
+    'worklet';
+    TranslateY.value = withPause(withDelay(3000,withRepeat(withSequence(
       withTiming(-100, {duration:1000}),
       withTiming(100, {duration:1000}),
       withTiming(0, {duration:1000})
-    ), -1))
+    ), -1)), paused)
   }
   useEffect(() => {
+    'worklet';
     TranslateFunc()
   })
+  // Cancel Animation
+  const cancelTranslateAnimation = () => {
+    'worklet';
+    cancelAnimation(TranslateY);
+  }
+ 
   return (
     <View className=' flex justify-center items-center min-h-screen'>
-      <Animated.View style={animTranslate} className=' bg-purple-500 w-28 h-28 rounded-full'/>
+      <Animated.View style={animTranslate} className=' bg-purple-500 w-28 h-28 rounded-full' />
+      <Pressable onPress={()=>cancelTranslateAnimation()} className=' p-4 rounded-xl bg-slate-800 absolute bottom-20 left-2'>
+        <Text className=' font-bold text-white'>Cancel</Text>
+      </Pressable>
+      <Pressable onPress={()=>toggle()} className=' p-4 rounded-xl bg-slate-800 absolute bottom-20 right-2'>
+        <Text className=' font-bold text-white'>Pause/Play</Text>
+      </Pressable>
     </View>
   )
 }
